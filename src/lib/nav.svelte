88fa17b116289356ee31d3c7e0f8dce0ef3c24b9
Anders Bille <anders.bille.lind@gmail.com>
2022-03-09 14:41:36 +0100

    Nav and Tailwind

---
+++ src/lib/nav.svelte
@@ -0,0 +1,12 @@
+<script>
+    import SetTheme from './setTheme.svelte' 
+</script>
+
+<nav class="flex items-center justify-between max-w-5xl mx-auto mt-8">
+    <div class="flex items-center gap-x-4">
+        <a href="/" class="hover:underline text-lg">Anders Bille Lind</a>
+        <a href="/blog" class="hover:underline ">Blog</a>
+        <a href="/resume" class="hover:underline ">Resume</a>
+    </div>
+    <SetTheme/>
+</nav>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '../app.css';
+	import Nav from '../lib/nav.svelte';
+
+	/* Theme */
+	import { onMount } from 'svelte';
+	import { theme } from '../stores/storage';
+	onMount(() => {
+	    theme.useLocalStorage();
+		if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+			theme.set({ ...$theme, mode: 'light' });
+		}
+	});
+</script>
+
+<Nav />
+
+<main class="max-w-5xl mx-auto">
+	<slot />
+</main>
+
+<footer />
