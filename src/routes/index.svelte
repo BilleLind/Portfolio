ebf9cb30e3c3a5a514a54b690d4c4c4b9d2f50a2
Anders Bille <anders.bille.lind@gmail.com>
2022-03-14 13:32:58 +0100

    landing page - first section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,8 @@
 <script>
+	import Projects from '../lib/icons/projects.svelte';
+	import Heart from '../lib/icons/heart.svelte';
+	import Journal from '../lib/icons/journal.svelte';
+
 	let currentTime = new Date().getHours();
 	let greet;
 	if (currentTime <= 6) {
@@ -12,7 +16,30 @@
 	}
 </script>
 
-<section class="h-screen max-w-5xl mx-auto mt-32 text-center">
-	<h2 class="text-base">{greet} I'm Bille!</h2>
-    
+<section class="max-w-5xl mx-auto pt-32 text-center">
+	<h2 class="text-lg">{greet} I'm <span class="accent font-bold">Bille!</span></h2>
+
+	<div class="mt-10 flex justify-center gap-x-6">
+		<a
+			href="#projects"
+			class="p-2 rounded-xl flex gap-x-2 items-center text-accent bg-slate-900 shadow-md shadow-black hover:shadow-accent transition duration-500 "
+		>
+			<Projects /> Projects
+		</a>
+		<a
+			href="#interest"
+			class="p-2 rounded-xl flex gap-x-2 items-center text-accent bg-slate-900 shadow-md shadow-black hover:shadow-accent transition duration-500"
+		>
+			<Heart styling="text-red-600" /> Interest
+		</a>
+		<a
+			href="#experience"
+			class="p-2 rounded-xl flex gap-x-2 items-center text-accent bg-slate-900 shadow-md shadow-black hover:shadow-accent transition duration-500"
+		>
+			<Journal styling="text-purple-400" /> Experience
+		</a>
+	</div>
+</section>
+
+<section class="">
 </section>
