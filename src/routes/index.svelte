2fa5bf179ff751567534022510e65b534a961d83
Anders Bille <anders.bille.lind@gmail.com>
2022-03-16 18:07:07 +0100

    Removal of the 3 links
    decided against having the 3 links and merged the next section into it instead

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,48 +26,24 @@
 <section class="pt-32">
 	<h2 class="text-lg text-center">{greet} I'm <span class="accent font-bold">Bille!</span></h2>
 
-	<div class="mt-10 flex flex-col md:flex-row items-center justify-center gap-6 ">
-		<a
-			href="#projects"
-			aria-label="Go to projects"
-			class="p-3 rounded-xl flex gap-x-2 items-center text-accent bg-slate-900 dark:shadow-lg dark:shadow-black dark:hover:shadow-accent transition duration-500  "
-		>
-			<Projects styling="" /> Projects
-		</a>
-		<a
-			href="#interests"
-			aria-label="Go to interests"
-			class="p-3 rounded-xl flex gap-x-2 items-center text-accent bg-slate-900 dark:shadow-lg dark:shadow-black dark:hover:shadow-accent transition duration-500 "
-		>
-			<Heart styling="text-red-600" /> Interest
-		</a>
-		<a
-			href="#experience"
-			aria-label="Go to experience"
-			class="p-3 rounded-xl flex gap-x-2 items-center text-accent bg-slate-900 dark:shadow-lg dark:shadow-black dark:hover:shadow-accent transition duration-500 "
-		>
-			<Journal styling="text-purple-400" /> Experience
-		</a>
-	</div>
-</section>
-
-<section class="flex flex-col xl:flex-row gap-y-12 justify-around mt-20">
-	<div class="flex-1 text-center">
-		<h2 class="text-base md:text-lg font-medium whitespace-nowrap">
-			I'm a Junior Full-Stack Developer,
-		</h2>
-		<h3 class="">Focused on creating efficient applications.</h3>
-		<p />
-	</div>
-	<div class="flex-1 flex justify-center gap-x-8 items-center">
-		<a href="https://github.com/BilleLind" aria-label="Github link"
-			><Github styling="w-10 h-10 hover:text-accent" /></a
-		>
-		<a href="https://www.linkedin.com/in/anders-bille-lind-185074200/" aria-label="linkedin link"
-			><Linkedin styling="w-10 h-10 hover:text-accent" /></a
-		>
-		<a href="mailto:anders.bille.lind@gmail.com" aria-label="Mailto link"
-			><Mail styling="w-10 h-10 hover:text-accent" /></a
-		>
+	<div class="flex flex-col xl:flex-row gap-y-12 justify-around mt-20">
+		<div class="flex-1 text-center">
+			<h2 class="text-base md:text-lg font-medium whitespace-nowrap">
+				I'm a Junior Full-Stack Developer,
+			</h2>
+			<h3 class="">Focused on creating efficient applications.</h3>
+			<p />
+		</div>
+		<div class="flex-1 flex justify-center gap-x-8 items-center">
+			<a href="https://github.com/BilleLind" aria-label="Github link"
+				><Github styling="w-10 h-10 hover:text-accent" /></a
+			>
+			<a href="https://www.linkedin.com/in/anders-bille-lind-185074200/" aria-label="linkedin link"
+				><Linkedin styling="w-10 h-10 hover:text-accent" /></a
+			>
+			<a href="mailto:anders.bille.lind@gmail.com" aria-label="Mailto link"
+				><Mail styling="w-10 h-10 hover:text-accent" /></a
+			>
+		</div>
 	</div>
 </section>
