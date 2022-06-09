d34200d8534d9b50950c9c91f827e3d4f18acced
Anders Bille <anders.bille.lind@gmail.com>
2022-06-09 23:29:16 +0200

    Migration to Vue(Nuxt)

--- src/lib/components/tag.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let styling, link;
-</script>
-
-<a href={link} rel="norefferor" target="_blank"
-	class="bg-primary-dark-900 text-xs flex items-center justify-center py-1 px-2 rounded-xl w-fit whitespace-nowrap {styling}"
->
-	<slot />
-</a>
