155e7f4dc609f5479a73702783d34b87d974642d
Anders Bille <anders.bille.lind@gmail.com>
2022-06-13 23:14:32 +0200

    Unocss, reset and Vueuse setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        '@vueuse/nuxt'
-    ]
+	modules: ['@unocss/nuxt', '@vueuse/nuxt'],
+	unocss: {
+		uno: true,
+		icons: true,
+		attributify: true,
+	},
+	css: ['@unocss/reset/tailwind.css'],
 })
