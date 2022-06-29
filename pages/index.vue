5e328c8db20774d29ed8637b6b432bb0f85d6c4b
Anders Bille <anders.bille.lind@gmail.com>
2022-06-29 22:24:11 +0200

    feat: first section

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,60 @@
 <script>
-// This will also work in `<script setup>`
 definePageMeta({
   layout: "default",
 });
 </script>
 
 <template>
-    <div text-center text-2xl mt-20>
-        Index page
-    </div>
+  <main text-2xl mt-20 max-w-80% sm:max-w-60% mx-auto>
+
+    <p lg:ml-10% text-xl sm:text-2xl md:text-3xl lg:text-4xl>{{ getGreeting() }}</p>
+    <h1 heading ml-5% lg:ml-15%> I am Anders Bille Lind,
+      <br><em text-purple-700 dark:text-purple-600 ml-5%>a Developer</em>,
+      <br><em ml-10%> a Father</em> <br sm:display-none><span ml-15% sm:ml-0 text-base md:text-lg lg:text-xl op-70>
+        and bunch of other things
+      </span>
+    </h1>
+
+
+    <section mt-20>
+      <h2 heading boder-2 border-b border-dashed>Tool Box</h2>
+
+      <div grid grid-cols-4 m-3 gap-6>
+        <Icon logo="laravel">Laraval</Icon>
+        <Icon logo="vue">Vue</Icon>
+         <Icon logo="nuxt">Nuxt</Icon>
+        <Icon logo="fastify-icon">Fastify</Icon>
+        <Icon logo="nodejs-icon">Node</Icon>
+        <Icon logo=""></Icon>
+        <Icon logo=""></Icon>
+        <Icon logo=""></Icon>
+        <Icon logo=""></Icon>
+      </div>
+    </section>
+
+
+
+    <section mt-20>
+      <h2 heading>Projects</h2>
+
+    </section>
+
+    <section mt-20>
+      <h2 heading>Articles</h2>
+
+    </section>
+
+
+
+  </main>
 </template>
+
+
+  Index page
+
+        opbygning:
+        - intro + billede evt. knap til en about side
+          => aboute side => far + datamatiker osv, nævn skoler, arbejder og anden erfaring eller oplevelser
+        - projekter
+        - færdigheder => med eksempler, evt. visning med hover eller læs mere.away
+        - toolbox => bærbar, kontor osv.
