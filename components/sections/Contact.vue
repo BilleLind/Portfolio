709517dc68cbeea7a7e52c10c80ea1132853b530
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-11-02 23:45:55 +0100

    Contact, CardLink inital styling

---
+++ components/sections/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="">
+    <h3 class="">Contact</h3>
+    <div class="grid grid-cols-2 gap-4">
+      <NuxtLink to="twitter">
+        <CardLink header="Twitter" description="desc" />
+      </NuxtLink>
+
+    </div>
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,8 @@
 <template>
   <h2 text-2xl text-accent text-center mt-20>About page in progress</h2>
+
+  <SectionsContact class="section"/>
+
 </template>
 
 <script setup>
