3b01e352a1f6d86ccf48ba88fe2e4eba0a3ed137
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-11-03 09:26:32 +0100

    feat: initial contacts methods

--- components/CardLink.vue
+++ components/CardLink.vue
@@ -1,16 +1,16 @@
 <script setup>
 defineProps({
   header: String,
-  description: String,
+  description: String
 })
 </script>
 <template>
   <article 
-    class="w-full flex justify-between border-2 rounded-lg items-center p-1 md:p-2 group hover:border-accent transition ease-in duration-500 ">
+    class="w-full flex justify-between border-2 rounded-lg items-center p-1 md:p-2 hover:border-accent transition ease-in duration-500 ">
     <div>
       <h4 text-xl>{{ header }}</h4>
       <p opacity-75>{{ description }}</p>
     </div>
-    <div i-carbon-logo-twitter w-10 h-10 group-hover:animate-bounce duraction-none> </div>
+    <slot/>
   </article>
 </template>
